2cf07a163803048a33cc86436f4ca6768792037a
tifanny04 <154246344+tifanny04@users.noreply.github.com>
2024-03-19 21:52:37 -0600

    punto5 2.0

--- objetos.js
+++ objetos.js
@@ -79,20 +79,20 @@ class ProgramWindow{
             this.size.height=tam.height
     }
     //Punto 5
-    move(movewindow) {
-        if (movewindow.x < 0)
+    move(newPosition) {
+        if (newPosition.x < 0)
             this.position.x = 0;
-        else if (movewindow.x + this.size.width > this.screenSize.width)
+        else if (newPosition.x + this.size.width > this.screenSize.width)
             this.position.x = this.screenSize.width - this.size.width;
         else
-            this.position.x = movewindow.x;
+            this.position.x = newPosition.x;
 
-        if (movewindow.y < 0)
+        if (newPosition.y < 0)
             this.position.y = 0;
-        else if (movewindow.y + this.size.height > this.screenSize.height)
+        else if (newPosition.y + this.size.height > this.screenSize.height)
             this.position.y = this.screenSize.height - this.size.height;
         else
-            this.position.y = movewindow.y;
+            this.position.y = newPosition.y;
     }
 }
 
@@ -120,3 +120,9 @@ console.log("----------------------------------------------------");
 const newSize = new Size(600, 400);
 programWindow.resize(newSize);
 console.log(programWindow.size);
+console.log("----------------------------------------------------");
+//Punto 5
+const newPosition = new Position(50, 100);
+programWindow.move(newPosition); 
+console.log(programWindow.Position);
+console.log("----------------------------------------------------");
